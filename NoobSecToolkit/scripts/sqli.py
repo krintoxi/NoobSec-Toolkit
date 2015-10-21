643d0ff27dc89f63bd61db79281885e5b9c0f814
The_Chosen_One <leo@libertadpress.com>
2015-10-21 04:18:10 -0400

    General Toolkit Updates

--- NoobSecToolkit/scripts/sqli.py
+++ NoobSecToolkit/scripts/sqli.py
@@ -8,4 +8,4 @@ print "SQLI Injector"
 
 target = raw_input('SQLI Vulnerable Target: ')
 
-cmd1 = os.system ('python '+'tools/sqli/sqlmap.py -u' +target+' --tor --tor-type=SOCKS5 --check-tor --tor-port=9050 --random-agent --level=3 --risk=3 --threads=2')
+cmd1 = os.system ('python '+'tools/inject/sqlmap.py -u' +target+' --tor --tor-type=SOCKS5 --check-tor --tor-port=9050 --random-agent --level=3 --risk=3 --threads=2')
